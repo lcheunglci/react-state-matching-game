66ce896d6923e337974477ec85394bb57edb1d7e
Lawrence LCI <lcheunglci@gmail.com>
2022-02-07 22:46:49 -0800

    Complete module 3 - Handling click events

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,7 +6,7 @@ const Tile = (props) => {
   const colorStyle = props.selected || props.matched ? { backgroundColor: props.color } : null;
 
   return (
-    <div className='Tile' style={colorStyle}>
+    <div className='Tile' style={colorStyle} onClick={() => props.handleTileClicked(props.id, props.color)}>
       {props.selected  || props.matched ? <props.svg /> : null }
     </div>
   )
